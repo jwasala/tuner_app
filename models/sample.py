e1bf3044b7794ee70294b07e8f2a830ceaa2b8f4
jwasala <jakubwasala1@gmail.com>
2021-04-24 11:36:30 +0200

    Initial commit

---
+++ models/sample.py
@@ -0,0 +1,11 @@
+class Sample:
+    def __init__(self, data):
+        self.data = data
+
+    @property
+    def frequency(self):
+        pass
+
+    @property
+    def closest_note(self):
+        pass
