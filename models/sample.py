7fedf681cc0b679b43598713bce2e5bcc946294f
jwasala <jakubwasala1@gmail.com>
2021-04-25 17:01:28 +0200

    Properties for Sample class.

--- models/sample.py
+++ models/sample.py
@@ -2,8 +2,24 @@ import numpy as np
 
 
 class Sample:
-    def __init__(self, data: np.ndarray):
+    def __init__(self, rate: int, data: np.ndarray):
+        self.rate = rate
         self.data = data
 
+    @property
+    def duration(self):
+        return len(self.data) / self.rate
+
+    @property
+    def power(self):
+        """
+        :return: Signal power of the sample.
+        """
+        return np.sum([x ** 2 for x in self.data]) / len(self.data)
+
     def estimate_frequency(self) -> float:
-        pass
+        """
+        Estimates frequency of the sample using Harmonic Product Spectrum.
+
+        :return: Estimated frequency.
+        """
