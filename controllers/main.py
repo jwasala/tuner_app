2a243d9aaf2b8cd7b2027d64d5a3340eb848a73c
jwasala <jakubwasala1@gmail.com>
2021-05-07 21:25:26 +0200

    Packed driver methods into the App class

--- controllers/main.py
+++ controllers/main.py
@@ -2,16 +2,18 @@ from controllers.stream import Stream
 from models.pitch import Pitch
 
 
-def model_to_view(pitch: Pitch, freq: float) -> None:
-    print(pitch, freq, 'Hz')
+class App:
+    @classmethod
+    def model_to_view(cls, pitch: Pitch, freq: float) -> None:
+        print(pitch, freq, 'Hz')
 
+    @classmethod
+    def main(cls):
+        stream = Stream(cls.model_to_view)
+        stream.start()
 
-def main():
-    stream = Stream(model_to_view)
-    stream.start()
-
-    input('Press any key to exit')
+        input('Press any key to exit')
 
 
 if __name__ == '__main__':
-    main()
+    App.main()
--- models/constants.py
+++ models/constants.py
@@ -1,3 +1,6 @@
+from models.pitch import Pitch, Note
+
+
 A4_PITCH = 440
 
 _12TH_ROOT_OF_2 = 1.05946309436
@@ -9,3 +12,24 @@ BLOCK_SIZE = 44100
 HIGH_FREQUENCY = 700
 
 LOW_FREQUENCY = 80
+
+TUNINGS = {
+    'guitar': {
+        'standard': {
+            Pitch(Note.E, 2),
+            Pitch(Note.A, 2),
+            Pitch(Note.D, 3),
+            Pitch(Note.G, 3),
+            Pitch(Note.B, 3),
+            Pitch(Note.E, 4)
+        },
+        'half-step down': {
+            Pitch(Note.DSharp, 2),
+            Pitch(Note.GSharp, 2),
+            Pitch(Note.CSharp, 3),
+            Pitch(Note.FSharp, 3),
+            Pitch(Note.ASharp, 3),
+            Pitch(Note.DSharp, 4)
+        }
+    }
+}
